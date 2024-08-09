985537fe7fdfa270bae2508a7c63745e723ad015
shahd <shoshomohy13@gmail.com>
2024-08-09 23:35:09 +0300

    Adding const

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class _MyAppState extends State<MyApp> {
     return (MaterialApp(debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text(
+          title: const Text(
             "Points Counter",
             style: TextStyle(color: Colors.white, fontSize: 20),
           ),
